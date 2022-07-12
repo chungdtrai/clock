95cd59aab683eecca819fb1f8c4be6723826a331
Doi Thanh Chung <doithanhchung20011110@gmail.com>
2022-07-12 12:27:15 +0700

    update1

--- maincl.js
+++ maincl.js
@@ -1,46 +1,55 @@
-var day=null, hour=null, minute=null, tick=null;
-// day = document.getElementById("day").value
-// hour = document.getElementById("hour").value
-// minute = document.getElementById("minute").value
-// tick = document.getElementById("tick").value
-// day = parseInt(document.getElementById("day").value)
-// hour = parseInt(document.getElementById("hour").value)
-// minute = parseInt(document.getElementById("minute").value)
-// tick = parseInt(document.getElementById("tick").value)
-// console.log(day, hour, minute, tick)
-var settime;
-function start(){
-    console.log(day, hour, minute, tick)
-    if(day === null){
+let day = null, hour = null, minute = null, tick = null;
+let settime;
+var startbut = document.getElementById("startbut");
+var stopbut = document.getElementById("stopbut");
+function start() {
+    clicktime++;
+    console.log(clicktime);
+    if (day === null) {
         day = document.getElementById("day").value
         hour = document.getElementById("hour").value
         minute = document.getElementById("minute").value
         tick = document.getElementById("tick").value
     }
-    if(tick === -1){
-        minute -=1;
+    if (tick === -1) {
+        minute -= 1;
         tick = 59;
     }
-    if(minute === -1){
+    if (minute === -1) {
         hour -= 1;
         minute = 59;
     }
-    if(hour === -1){
-        day -=1;
+    if (hour === -1) {
+        day -= 1;
         hour = 23;
-    } 
-    if(day == -1){
+    }
+    if (day == -1) {
         alert("TIME OUT");
     }
-    document.getElementById('t').innerText = tick.toString();
-    document.getElementById('m').innerText = minute.toString();
-    document.getElementById('h').innerText = hour.toString();
-    document.getElementById('d').innerText = day.toString();
-    settime = setTimeout(function(){
+    let time1, time2, time3, time4;
+    tick < 10 ? time1 = "0" + tick : time1 = tick;
+    minute < 10 ? time2 = "0" + minute : time2 = minute;
+    hour < 10 ? time3 = "0" + hour : time3 = hour;
+    document.getElementById('t').innerText = time1;
+    document.getElementById('m').innerText = time2;
+    document.getElementById('h').innerText = time3;
+    document.getElementById('d').innerText = day;
+    settime = setTimeout(function () {
         tick--;
         start();
-    },1000) 
+        startbut.onclick = function (e) { }
+    }, 1000)
 }
-function stop(){
+function stop() {
     clearTimeout(settime)
+    startbut.onclick = function (e) {
+        start();
+    }
 }
+startbut.onclick = function (e) {
+    start();
+}
+stopbut.onclick = function (e) {
+    stop()
+}
+
